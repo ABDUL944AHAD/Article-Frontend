9c1bc198b83013b66fa6902ffa6a845a4c64cd9e
ABDUL944AHAD <7249ahad@gmail.com>
2025-09-13 12:04:12 +0500

    changed the railway url

--- src/config/Config.js
+++ src/config/Config.js
@@ -3,4 +3,4 @@ const isLocal = window.location.hostname === "localhost";
 
 export const API_BASE_URL = isLocal
     ? "http://localhost:5000" // Local backend
-    : "https://article-backend-deployibg.up.railway.app"; // Railway backend
+    : "https://article-backend-production-3153.up.railway.app/"; // Railway backend
